ad8ea119d06a01e75fee96b4efb4b47d406e4ab1
Obidur Rahman <obidur.shawal@gmail.com>
2024-07-10 17:30:04 +0600

    new file:   .ipynb_checkpoints/notebook-checkpoint.ipynb 	new file:   api/index.py 	new file:   model.pkl 	modified:   notebook.ipynb 	new file:   requirements.txt 	new file:   static/css/styles.css 	new file:   static/js/scripts.js 	new file:   templates/index.html 	new file:   tfidf_vectorizer.pkl 	new file:   vercel_app.py

---
+++ api/index.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template
+import pickle
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+# Initialize NLTK tools
+nltk.download('punkt')
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+stemmer = SnowballStemmer('english')
+
+# Load the trained model and vectorizer
+with open('model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+with open('tfidf_vectorizer.pkl', 'rb') as vectorizer_file:
+    tfidf_vectorizer = pickle.load(vectorizer_file)
+
+# Preprocessing function
+def preprocess_text(text):
+    text = re.sub(r'<[^>]+>', '', text)
+    text = re.sub(r'[^\w\s]', '', text)
+    text = text.lower()
+    words = word_tokenize(text)
+    words = [word for word in words if word not in stop_words]
+    words = [stemmer.stem(word) for word in words]
+    return ' '.join(words)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    review = request.form['review']
+    processed_review = preprocess_text(review)
+    review_tfidf = tfidf_vectorizer.transform([processed_review])
+    prediction = model.predict(review_tfidf)[0]
+    return render_template('index.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
